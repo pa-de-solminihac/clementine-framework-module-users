d6c86fefec9ea0c9c4276a7185b41f9683b6357e
Pierre-Alexis de Solminihac <pa@solminihac.fr>
2013-02-05 01:53:35 +0100

    restoring users-2.0 branch

--- view/users/mail_renew_pass_body.php
+++ view/users/mail_renew_pass_body.php
@@ -2,13 +2,12 @@ Bonjour, <br />
 <br />
 Une demande de renouvellement de mot de passe a été reçue pour votre compte sur <a href="<?php echo __WWW__; ?>"><?php echo Clementine::$config['clementine_global']['site_name']; ?></a><br />
 <br />
-Cette demande ayant été confirmée, votre mot de passe a été renouvelé.<br />
+Cette demande ayant été confirmée, votre mot de passe a été renouvellé.<br />
 <br />
 <strong>Votre nouveau mot de passe est :</strong><br />
-__CLEMENTINE_MAIL_ANONYMIZE_START__
 <?php
-echo $data['newpass'];
-?>__CLEMENTINE_MAIL_ANONYMIZE_STOP__<br />
+    echo $data['newpass'];
+?><br />
 <br />
 Bonne journée,<br />
 <hr />
