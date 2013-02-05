690b57d7f0fe981247230b511dbff97a3cb48b23
Pierre-Alexis de Solminihac <pa@solminihac.fr>
2013-02-05 01:52:40 +0100

    restoring users-1.10 branch

--- view/users/create.php
+++ view/users/create.php
@@ -1,5 +1,5 @@
 <div class="form_users_create">
-    <form id="clementine_users_add" name="clementine_users_add" method="post" action="<?php echo __WWW__; ?>/users/validuser" enctype="multipart/form-data">
+    <form name="add_user" method="post" action="<?php echo __WWW__; ?>/users/validuser" enctype="multipart/form-data">
 <?php 
     $this->getBlock('users/fieldsform', $data);
 ?>
