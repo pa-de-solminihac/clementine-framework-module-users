a7f89146429d91d499b2c66132570c4d4d4b43cf
Pierre-Alexis de Solminihac <pa@solminihac.fr>
2013-02-05 01:52:57 +0100

    restoring users-1.14 branch

---
+++ view/users/index.php
@@ -0,0 +1,49 @@
+<div class="form_users_index">
+    <a class="form_users_index_add" title="créer" href="<?php echo __WWW__; ?>/users/create" >
+        <img src="<?php echo __WWW_ROOT_USERS__; ?>/skin/images/add.png" />
+        Ajouter un utilisateur
+    </a>
+    <table class="clementine_users_index clementine-dataTables" id="clementine_users_index">
+        <thead>
+            <tr>
+                <th class="col1"> Nom d'utilisateur </th>
+                <th class="col2"> Actions</th>
+            </tr>
+        </thead>
+<?php 
+if (isset($data['users']) && is_array($data['users']) && count($data['users'])) {
+?>
+        <tbody>
+<?php
+    foreach ($data['users'] as $id => $users) {
+?>
+            <tr>
+                <td class="col1"><a title="modifier" href="<?php echo __WWW__; ?>/users/edit?id=<?php echo $id; ?>" >
+                        <?php echo $users['login']; ?>
+                </a></td>
+                <td class="col2">
+                    <a title="modifier" href="<?php echo __WWW__; ?>/users/edit?id=<?php echo $id; ?>" >
+                        <img src="<?php echo __WWW_ROOT_USERS__; ?>/skin/images/edit.png" />
+                    </a>
+                    <a title="supprimer" onclick="return(confirm('Etes-vous sûr de vouloir supprimer cet utilisateur ?'));" href="<?php echo __WWW__; ?>/users/delete?id=<?php echo $id; ?>" >
+                        <img src="<?php echo __WWW_ROOT_USERS__; ?>/skin/images/delete.png" />
+                    </a>
+                </td>
+            </tr>
+<?php 
+    }
+?>
+        </tbody>
+<?php
+} else {
+?>
+        <tbody>
+            <tr>
+                <td colspan="4">Il n'y a aucun utilisateur. </td>
+            </tr>
+        </tbody>
+<?php
+}
+?>
+    </table>
+</div>
