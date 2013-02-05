28483557d590fe4aabb64ed548f4c36170ca81d4
Pierre-Alexis de Solminihac <pa@solminihac.fr>
2013-02-05 01:52:24 +0100

    restoring users-1.7 branch

---
+++ ctrl/usersHookController.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * usersHookController 
+ * 
+ * @package 
+ * @version $id$
+ * @copyright 
+ * @author Pierre-Alexis <pa@quai13.com> 
+ * @license 
+ */
+class usersHookController extends usersHookController_Parent
+{
+    /**
+     * before_first_getRequest : fonction appelee avant le premier appel a getRequest()
+     * 
+     * @access public
+     * @return void
+     */
+    function before_first_getRequest()
+    {
+        // appelle le hook parent s'il existe
+        parent::before_first_getRequest();
+        // utilisation du hook 'before_first_getRequest'
+        $this->getModel('users')->getAuth();
+    }
+
+}
+?>
