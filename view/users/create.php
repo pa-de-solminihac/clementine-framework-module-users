6333f45d0d0765e77ae1bd6ba1237a9947d1e25e
Pierre-Alexis de Solminihac <pa@solminihac.fr>
2013-02-05 01:51:51 +0100

    restoring users-1.0 branch

--- view/users/create.php
+++ view/users/create.php
@@ -1,7 +1,33 @@
 <div class="form_users_create">
-    <form id="clementine_users_add" name="clementine_users_add" method="post" action="<?php echo __WWW__; ?>/users/validuser" enctype="multipart/form-data">
-<?php 
-    $this->getBlock('users/fieldsform', $data);
-?>
+    <form method="post" action="<?php echo __WWW__; ?>/users/validuser" enctype="multipart/form-data">
+        <input type="hidden" name="id" value="" />
+        <input type="hidden" name="mdpOK" value="0" />
+
+        <table class="clementine_users_create" id="clementine_users_create">
+            <thead>
+                <tr>
+                    <th class="col1">Adresse email </th>
+                    <th class="col2">Mot de passe</th>
+                    <th class="col3">Confirmation du mot de passe</th>
+                    <th class="col4"></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td class="col1 form_users_create_mail">
+                        <input type="text" name="login" size="20" value="" />
+                    </td>
+                    <td class="col2 form_users_create_mdp">
+                        <input type="password" name="password" size="20" value="" />
+                    </td>
+                    <td class="col3 form_users_create_mdp_confirm">
+                        <input type="password" name="password_conf" size="20" value="" />
+                    </td>
+                    <td class="col4 form_users_create_submit">
+                        <input type="submit" name="valider" value="valider" />
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </form>
 </div>
--- view/users/edit.php
+++ view/users/edit.php
@@ -2,12 +2,49 @@
 <?php
 $ns = $this->getModel('fonctions');
 if (isset($data['user'])) {
-    $user = $data['user'];
+    $user = $data['user']; 
 ?>
-    <form id="clementine_users_add" name="clementine_users_add" method="post" action="<?php echo __WWW__; ?>/users/validuser" enctype="multipart/form-data">
-<?php
-    $this->getBlock('users/fieldsform', $data);
-?>
+    <form name="add_user" method="post" action="<?php echo __WWW__; ?>/users/validuser" enctype="multipart/form-data">
+        <input type="hidden" name="id" value="<?php
+    if ($ns->ifGet('int', 'id')) { 
+        echo $ns->ifGet('int', 'id'); 
+    } else { 
+        echo '0'; 
+    } 
+?>" />
+        <input type="hidden" name="mdpOK" value="<?php 
+if ($user['password']) { 
+    echo $user['password']; 
+} else { 
+    echo '0'; 
+} 
+?>" />
+        <table class="clementine_users_edit" id="clementine_users_edit">
+            <thead>
+                <tr>
+                    <th class="col1">Adresse email </th>
+                    <th class="col2">Mot de passe</th>
+                    <th class="col3">Confirmation du mot de passe</th>
+                    <th class="col4"></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td class="col1 form_users_edit_mail">
+                        <input type="text" name="login" size="20" value="<?php echo $user['login']; ?>" />
+                    </td>
+                    <td class="col2 form_users_edit_mdp">
+                        <input type="password" name="password" size="20" value="<?php echo $user['password']; ?>" />
+                    </td>
+                    <td class="col3 form_users_edit_mdp_confirm">
+                        <input type="password" name="password_conf" size="20" value="<?php echo $user['password']; ?>" />
+                    </td>
+                    <td class="col4 form_users_edit_submit">
+                        <input type="submit" name="valider" value="valider" />
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </form>
 <?php
 } else {
