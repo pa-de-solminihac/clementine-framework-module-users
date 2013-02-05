3a75b311a8ed80bfca8859cd55034726f8742df7
Pierre-Alexis de Solminihac <pa@solminihac.fr>
2013-02-05 01:56:54 +0100

    restoring users-3.7 branch

--- view/users/validuser.php
+++ view/users/validuser.php
@@ -1,13 +1,13 @@
 <?php
 if (isset($data['errors'])) {
-    if (!$request->AJAX) {
+    if (!$request['AJAX']) {
 ?>
 <div class="form_users_validnew">
     <div class="error">
 <?php
     }
     $this->getBlock('users/errors/validuser', $data);
-    if (!$request->AJAX) {
+    if (!$request['AJAX']) {
 ?>
     </div>
     <div class="spacer"></div>
