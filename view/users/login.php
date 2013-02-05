97445cde4f2e70778a69c155dcb77d240deeac41
Pierre-Alexis de Solminihac <pa@solminihac.fr>
2013-02-05 01:54:41 +0100

    restoring users-2.16 branch

--- view/users/login.php
+++ view/users/login.php
@@ -1,5 +1,6 @@
 <?php
-$current_url = $request->EQUIV[$request->LANG];
+$request = $this->getRequest();
+$current_url = $request['EQUIV'][$request['LANG']];
 ?>
 <div class="form_users_login">
 <h1>
@@ -11,8 +12,8 @@ if (isset($data['message'])) {
 </h1>
 <form action="<?php echo __WWW__; ?>/users/login?url_retour=<?php echo (isset($data['url_retour'])) ? urlencode($data['url_retour']) : urlencode($current_url); ?>" method="post" >
     <p>
-        <label>Adresse e-mail</label><input type="text" id="form_users_login" name="login" value="" tabindex="1" />
-        <label>Mot de passe</label><input type="password" name="password" value="" tabindex="2" />
+        <label>Adresse e-mail</label><input type="text" id="form_users_login" name="login" value="" />
+        <label>Mot de passe</label><input type="password" name="password" value="" />
         <label>&nbsp;</label><input type="submit" value="Connexion" />
         <a href="<?php echo __WWW__; ?>/users/oubli">Mot de passe oublié</a>
     </p>
